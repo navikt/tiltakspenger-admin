59372e83a070b712823a5e81a79d09ea3c3395dc
Steffen Lien <steffen.lien@mimedi.no>
2023-01-25 09:55:55 +0100

    legg til deploy og workflows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>tiltakspenger-admin</h1>
+    </main>
+  );
 }
