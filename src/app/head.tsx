74494cc5531b2be12b379781c13002e6ae75b7ff
Steffen Lien <steffen.lien@mimedi.no>
2023-01-25 09:26:35 +0100

    init nextjs prosjekt

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Tiltakspenger Admin</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Administreringsflate for utviklere i Team TP"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
