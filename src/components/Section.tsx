8abdfff916abfe08369932f19fb572eb46b9b186
Steffen Lien <steffen.lien@mimedi.no>
2023-01-31 11:56:52 +0100

    små tilpasninger for å forbedre brukeropplevelsen

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -9,6 +9,8 @@ import {
     StatLabel,
     StatNumber,
     CardFooter,
+    Center,
+    Spinner,
 } from '@chakra-ui/react';
 
 export type SectionStat = { [key: string]: string };
@@ -16,14 +18,20 @@ export type SectionStat = { [key: string]: string };
 interface SectionProps extends React.PropsWithChildren {
     label: string;
     stats?: SectionStat;
+    isLoading: boolean;
 }
 
-function Section({ label, stats, children }: SectionProps) {
+function Section({ label, stats, children, isLoading }: SectionProps) {
     return (
         <Card my="4">
             <CardHeader>
                 <Heading>{label}</Heading>
             </CardHeader>
+            {isLoading && (
+                <Center>
+                    <Spinner />
+                </Center>
+            )}
             {stats && (
                 <CardBody>
                     <StatGroup>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,26 @@ import { Innsendinger } from '../types';
 import { useState } from 'react';
 
 function Home() {
-    const { data } = useSWR<Innsendinger>('/api/innsendinger/feiletogstoppet', fetcher);
+    const { data, isLoading } = useSWR<Innsendinger>('/api/innsendinger/feiletogstoppet', fetcher);
     const [resettLoading, setResettLoading] = useState(false);
     const toast = useToast();
+
     const handleResettInnsendinger = async () => {
         setResettLoading(true);
         await fetch('/api/innsendinger/resett/feiletogstoppet', { method: 'POST' }).then((res) => {
             setResettLoading(false);
             if (res.ok) {
                 toast({
-                    title: 'Resett',
-                    description: 'Resett av innsendinger var vellykket.',
+                    title: 'Trigget resett av innsendinger',
                     status: 'success',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            } else {
+                toast({
+                    title: 'Noe gikk galt',
+                    description: `${res.statusText}`,
+                    status: 'error',
                     duration: 5000,
                     isClosable: true,
                 });
@@ -26,8 +34,9 @@ function Home() {
     };
 
     return (
-        <Section label="Innsendinger" stats={data as SectionStat}>
+        <Section isLoading={isLoading} label="Innsendinger" stats={data as SectionStat}>
             <Button
+                isDisabled={isLoading}
                 isLoading={resettLoading}
                 loadingText="Resetter..."
                 onClick={handleResettInnsendinger}
